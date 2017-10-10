a43dc0f3bb82844c3ca450293cd41ca880d34b4a
Marco Carvalho <marcocarvalho.web@gmail.com>
2017-10-10 12:36:55 -0300

    Confguração da Base de Dados

--- config/banco_de_dados.php
+++ config/banco_de_dados.php
@@ -3,7 +3,7 @@
 return [
     'host' => '127.0.0.1',  // Servidor do Banco de dados - Padrão 'localhost' ('127.0.0.1')
     'port' => '3306',       // Porta do banco de dados MySQL - Padrão '3306'
-    'dbname' => 'esqueleto_aplicacao',  // Nome do banco de dados
+    'dbname' => 'forum',  // Nome do banco de dados
     'user' => 'root',   // Usuário com acesso ao banco de dados - Padrão 'root'
     'password' => 'MySQL#Root'  // Senha do usuário informado - Padrão na ETEC 'etec'
 ];
